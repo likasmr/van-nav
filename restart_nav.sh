6f995bfd9828ebd1fa5cf635d5a461062135ed7f
likasmr <likasmr1@gmail.com>
2025-03-04 12:27:26 +0800

    Update restart_nav.sh

--- restart_nav.sh
+++ restart_nav.sh
@@ -3,14 +3,21 @@
 WORK_DIR="/usr/home/$(whoami)/nav"
 NAV_PATH="$WORK_DIR/van-nav"
 LOG_FILE="$WORK_DIR/restart_log.txt"
-NAV_PORT=端口号
+PORT_FILE="$WORK_DIR/nav_port.conf"
+
+if [ ! -f "$PORT_FILE" ]; then
+    read -p "请输入 van-nav 运行端口号: " NAV_PORT
+    echo "$NAV_PORT" > "$PORT_FILE"
+else
+    NAV_PORT=$(cat "$PORT_FILE")
+fi
 
 if ! pgrep -f "$NAV_PATH" > /dev/null
 then
     cd "$WORK_DIR"
-    nohup ./van-nav -port $NAV_PORT > /dev/null 2>&1 &
-    
+    nohup ./van-nav -port "$NAV_PORT" > /dev/null 2>&1 &
+
     BEIJING_TIME=$(TZ=Asia/Shanghai date +"%Y-%m-%d %H:%M:%S")
-    
-    echo "van-nav 导航服务于 ${BEIJING_TIME} 重启" >> "$LOG_FILE"
+
+    echo "van-nav 导航服务于 ${BEIJING_TIME} 重启，端口号：$NAV_PORT" >> "$LOG_FILE"
 fi
