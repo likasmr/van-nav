7854f19dff1c95e6097daea55cc0b7b5847f37e5
likasmr <likasmr1@gmail.com>
2025-03-04 12:13:32 +0800

    Create restart_nav.sh

---
+++ restart_nav.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WORK_DIR="/usr/home/$(whoami)/nav"
+NAV_PATH="$WORK_DIR/van-nav"
+LOG_FILE="$WORK_DIR/restart_log.txt"
+NAV_PORT=端口号
+
+if ! pgrep -f "$NAV_PATH" > /dev/null
+then
+    cd "$WORK_DIR"
+    nohup ./van-nav -port $NAV_PORT > /dev/null 2>&1 &
+    
+    BEIJING_TIME=$(TZ=Asia/Shanghai date +"%Y-%m-%d %H:%M:%S")
+    
+    echo "van-nav 导航服务于 ${BEIJING_TIME} 重启" >> "$LOG_FILE"
+fi
